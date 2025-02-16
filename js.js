c6fcd5baf9b017c0b96d89c92f5d30e6a6322920
erboldev <arssovvv@gmail.com>
2025-02-16 12:10:32 +0600

    meduza

---
+++ js.js
@@ -0,0 +1,26 @@
+constvarswiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    loop:true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+
+
+
+
+
+
+
+
---
+++ js2.js
@@ -0,0 +1,160 @@
+let rol=document.querySelector(".rol")
+let piza=document.querySelector(".piza")
+let burger=document.querySelector(".burger")
+let drink=document.querySelector(".drink")
+let souse=document.querySelector(".souse")
+
+let herorol=document.querySelector(".herorol")
+let heropiza=document.querySelector(".heropiza")
+let heroburger=document.querySelector(".heroburger")
+let herodrink=document.querySelector(".herodrink")
+let herosouse=document.querySelector(".herosouse")
+
+let headerBtn1=document.querySelector(".headerBtn1")
+let modal =document.querySelector(".modal")
+let closebtn=document.querySelector(".closebtn")
+headerBtn1.addEventListener("click",()=>{
+    modal.style.display='block'
+})
+closebtn.addEventListener("click", ()=>{
+    modal.style.display='none'
+})
+
+
+rol.addEventListener("click",()=>{
+    herorol.style.display='block'
+    heropiza.style.display='none'
+    heroburger.style.display='none'
+    herodrink.style.display='none'
+    herosouse.style.display='none'
+})
+rol.addEventListener("click",()=>{
+    rol.style.background='silver'
+    piza.style.background='red'
+    burger.style.background='red'
+    drink.style.background='red'
+    souse.style.background='red'
+})
+rol.addEventListener("click",()=>{
+    rol.style.color='red'
+    piza.style.color='white'
+    burger.style.color='white'
+    drink.style.color='white'
+    souse.style.color='white'
+})
+
+
+
+
+
+
+piza.addEventListener("click",()=>{
+    heropiza.style.display='block'
+    herorol.style.display='none'
+    heroburger.style.display='none'
+    herodrink.style.display='none'
+    herosouse.style.display='none'
+})
+piza.addEventListener("click",()=>{
+    rol.style.background='red'
+    piza.style.background='silver'
+    burger.style.background='red'
+    drink.style.background='red'
+    souse.style.background='red'
+})
+piza.addEventListener("click",()=>{
+    rol.style.color='white'
+    piza.style.color='red'
+    burger.style.color='white'
+    drink.style.color='white'
+    souse.style.color='white'
+})
+
+
+
+
+
+
+
+
+
+
+
+burger.addEventListener("click",()=>{
+    heropiza.style.display='none'
+    herorol.style.display='none'
+    heroburger.style.display='burger'
+    herodrink.style.display='none'
+    herosouse.style.display='none'
+})
+burger.addEventListener("click",()=>{
+    rol.style.background='red'
+    piza.style.background='red'
+    burger.style.background='silver'
+    drink.style.background='red'
+    souse.style.background='red'
+})
+burger.addEventListener("click",()=>{
+    rol.style.color='white'
+    piza.style.color='white'
+    burger.style.color='red'
+    drink.style.color='white'
+    souse.style.color='white'
+})
+
+
+
+drink.addEventListener("click",()=>{
+    heropiza.style.display='none'
+    herorol.style.display='none'
+    heroburger.style.display='none'
+    herodrink.style.display='block'
+    herosouse.style.display='none'
+})
+drink.addEventListener("click",()=>{
+    rol.style.background='red'
+    piza.style.background='red'
+    burger.style.background='red'
+    drink.style.background='silver'
+    souse.style.background='red'
+})
+drink.addEventListener("click",()=>{
+    rol.style.color='white'
+    piza.style.color='white'
+    burger.style.color='white'
+    drink.style.color='red'
+    souse.style.color='white'
+})
+
+
+
+
+
+souse.addEventListener("click",()=>{
+    heropiza.style.display='block'
+    herorol.style.display='none'
+    heroburger.style.display='none'
+    herodrink.style.display='none'
+    herosouse.style.display='block'
+})
+souse.addEventListener("click",()=>{
+    rol.style.background='red'
+    piza.style.background='red'
+    burger.style.background='red'
+    drink.style.background='red'
+    souse.style.background='silver'
+})
+souse.addEventListener("click",()=>{
+    rol.style.color='white'
+    piza.style.color='white'
+    burger.style.color='white'
+    drink.style.color='white'
+    souse.style.color='red'
+})
+
+
+
+
+
+
+
